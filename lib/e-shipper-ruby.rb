23fbe7bc4c29af00661bfe392850861e038441e1
Daniel Gonzalez <dangt85@gmail.com>
2012-09-13 21:13:47 -0400

    adding builder methods

--- lib/e-shipper-ruby.rb
+++ lib/e-shipper-ruby.rb
@@ -7,11 +7,54 @@ module EShipperRuby
     request = post(url, build_request_quote_body(*options))
   end
 
-  def build_request_quote_body(*options)
+  def self.build_quote_request_body(options, packages)
     builder = Builder::XmlMarkup.new
     builder.instruct!
-    builder.EShipper("rxmlns" => "http://www.eshipper.net/XMLSchema",
-      "username" => options[:username], "password" => options[:username], "version" => "3.0.0")
+    builder.EShipper(options[:EShipper]) do |eshipper|
+      eshipper.QuoteRequest(options[:QuoteRequest]) do |quote|
+        quote.From(options[:From])
+        quote.To(options[:To])
+        quote.COD(options[:COD]) do |cod|
+          cod.CODReturnAddress(options[:CODReturnAddress])
+        end
+        quote.Packages(options[:Packages]) do |packs|
+          packages.each do |package|
+            packs.Package(package)
+          end
+        end
+        quote.Pickup(options[:Pickup])
+      end
+    end
+  end
+
+  def self.build_shipping_request_body(options, packages, references)
+    builder = Builder::XmlMarkup.new
+    builder.instruct!
+    builder.EShipper(options[:EShipper]) do |eshipper|
+      eshipper.ShippingRequest(options[:QuoteRequest]) do |quote|
+        quote.From(options[:From])
+        quote.To(options[:To])
+        quote.COD(options[:COD]) do |cod|
+          cod.CODReturnAddress(options[:CODReturnAddress])
+        end
+        quote.Packages(options[:Packages]) do |packs|
+          packages.each do |package|
+            packs.Package(package)
+          end
+        end
+        quote.Pickup(options[:Pickup])
+        quote.Payment(options[:Payment])
+        references.each do |reference|
+          quote.Reference(reference)
+        end
+        quote.CustomsInvoice(options[:CustomsInvoice]) do |invoice|
+          invoice.BillTo(options[:CustomsInvoice][:BillTo])
+          invoice.Contact(options[:CustomsInvoice][:Contact])
+          invoice.Item(options[:CustomsInvoice][:Item])
+          invoice.DutiesTaxes(options[:CustomsInvoice][:DutiesTaxes])
+        end
+      end
+    end
   end
 
   def post(url, request_body)
