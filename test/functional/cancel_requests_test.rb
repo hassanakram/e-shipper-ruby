6f5b289760c53761f5e042d4de8e9475c6b09863
damien imberdis <damien@ndstorefront.com>
2012-10-29 10:08:59 -0400

    updating functional tests

--- test/functional/cancel_requests_test.rb
+++ test/functional/cancel_requests_test.rb
@@ -4,13 +4,13 @@ class CancelRequestsTest  < Test::Unit::TestCase
 
   def test_e_shipper_cancel_shipping
     client = EShipper::Client.instance
-    options = { :order_id => '383363' }
+    options = { :order_id => '2143130' }
     
     response = client.cancel_shipping options
     assert_not_nil response
     assert response.is_a?(EShipper::CancelReply)
-    assert response.order_id
-    assert response.message
-    assert response.status
+    assert_equal '2143130', response.order_id
+    assert_equal 'Order has been cancelled!', response.message
+    assert_equal 'CANCELLED', response.status
   end
 end
--- test/functional/information_requests_test.rb
+++ test/functional/information_requests_test.rb
@@ -4,12 +4,12 @@ class InformationRequestsTest  < Test::Unit::TestCase
 
   def test_e_shipper_order_information
     client = EShipper::Client.instance
-    options = { :order_id => '383363' }
+    options = { :order_id => '2143130' }
     
     response = client.order_information options
     assert_not_nil response
     assert response.is_a?(EShipper::InformationReply)
-    assert response.order_id
+    assert_equal '2143130', response.order_id
     assert response.shipment_date
     assert response.status
     assert !response.history.empty?
